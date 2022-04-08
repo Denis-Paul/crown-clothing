3ce1ec4d9393cfa14cfd4ba71c495bd98b683354
Denis Paul <paul_denis100@yahoo.com>
2022-04-08 12:47:17 +0300

    added sign in component, firebase utils and brought in firebase libraries

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from 'firebase/auth';
+
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+} from 'firebase/firestore';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4wO1q9nULVd9MzE7sy_ZdseZmD1T-kHI",
+  authDomain: "crown-clothing-db-6287c.firebaseapp.com",
+  projectId: "crown-clothing-db-6287c",
+  storageBucket: "crown-clothing-db-6287c.appspot.com",
+  messagingSenderId: "861525236877",
+  appId: "1:861525236877:web:ad2612e8c35eaca52396c4"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account', // force account selection
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('Error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
